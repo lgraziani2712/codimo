5340b4ef4c9b3d4c8111802ea06af26af0d05d4c
Luciano Graziani <lgraziani2712@gmail.com>
2017-04-29 12:24:08 -0300

    🎨 Update definitions

--- flow-typed/blockly.js
+++ flow-typed/blockly.js
@@ -1,15 +1,25 @@
 /**
- * @license MIT License http://www.opensource.org/licenses/mit-license.php
  * @author Luciano Graziani @lgraziani2712
+ * @license {@link http://www.opensource.org/licenses/mit-license.php|MIT License}
+ *
+ * @flow
  */
 declare var Blockly: {
+  Blocks: {
+    [key: string]: {|
+      init(): void,
+    |},
+  },
   inject(elementId: string | HTMLElement, options: Object): Object,
-  Blocks: Object,
   JavaScript: {
-    workspaceToCode(workspace: Object): string,
+    ORDER_ATOMIC: string,
+    addReservedWords(...words: Array<string>): void;
     statementToCode(block: Object, name: string): string,
     valueToCode(block: Object, type: string, order: number): any,
-    ORDER_ATOMIC: string,
+    workspaceToCode(workspace: Object): string,
+
+    // Block excecutors
+    [key: string]: (block: Object) => string,
   },
   Events: {
     disableOrphans(event: Event): void,
--- flow-typed/globals.js
+++ flow-typed/globals.js
@@ -1,11 +1,18 @@
+/**
+ * @author Luciano Graziani @lgraziani2712
+ * @license {@link http://www.opensource.org/licenses/mit-license.php|MIT License}
+ *
+ * @flow
+ */
+
 declare var __VERSION__: string;
-declare var process: {
+declare var process: {|
   env: {
     [key: string]: ?string,
   },
-};
-declare var module: {
-  hot: {
-    accept: Function,
-  },
-};
+|};
+declare var module: {|
+  hot: {|
+    accept(dependencies: string | Array<string>, callback: (updatedDependencies: Object) => void): void,
+  |},
+|};
