12f2efb00712cdb1edfed603822698f6fd708fec
Luciano Graziani <lgraziani2712@gmail.com>
2017-05-06 19:59:54 -0300

    🚀 Add the first three working levels!!!
    - 🚀 [2017-05-06] Add the first three working levels!!!
    - 🚀 [2017-05-06] `gameMetadata`: update to v3.0.0.
      - Change `blocksData -> blocklyData`.
      - Split the metadata in two files. One for blockly definitions, another for games. This reduce the amount of metadata since blocks definitions are shared across multiple games.

---
+++ dev-elements/test/gameMetadata.js
@@ -0,0 +1,82 @@
+/**
+ * @author Luciano Graziani @lgraziani2712
+ * @license {@link http://www.opensource.org/licenses/mit-license.php|MIT License}
+ *
+ * @flow
+ */
+/* eslint-disable no-magic-numbers */
+
+export const blocklyData = {
+  elements: [{
+    define: 'block',
+    type: 'move_forward',
+  }, {
+    define: 'block',
+    type: 'move_backward',
+  }, {
+    define: 'block',
+    type: 'move_right',
+  }, {
+    define: 'block',
+    type: 'move_left',
+  }],
+  blockDefinitions: [{
+    name: 'move_forward',
+  }, {
+    name: 'move_backward',
+  }, {
+    name: 'move_right',
+  }, {
+    name: 'move_left',
+  }],
+  defaultElements: `
+    <block type="move_forward">
+      <next><block type="move_forward">
+        <next><block type="move_forward">
+          <next><block type="move_forward"/></next>
+        </block></next>
+      </block></next>
+    </block>
+  `,
+};
+
+export default {
+  mazeData: {
+    canvas: {
+      height: 500,
+      width: 450,
+    },
+    width: 5,
+    height: 5,
+    margin: 10,
+    size: 64,
+    path: [
+      '2,4',
+      '2,3',
+      '2,2',
+      '2,1',
+      '2,0',
+    ],
+    activePathBorders: [{
+      top: true,
+    }, {
+      top: true,
+      bottom: true,
+    }, {
+      top: true,
+      bottom: true,
+    }, {
+      top: true,
+      bottom: true,
+    }, {
+      top: true,
+      bottom: true,
+    }],
+    accesses: ['2,4'],
+    exits: ['2,0'],
+  },
+  numericLineData: {
+    statics: [1, null, 5],
+    accesses: [1],
+  },
+};
