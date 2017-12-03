f60122098eda812273b803b15f9364a703eaef2c
Luciano Graziani <lgraziani2712@gmail.com>
2017-12-03 17:44:50 -0300

    ⚙️ Add maze generator

---
+++ activities/MultipliersAndDivisors/__mocks__/metadata.js
@@ -0,0 +1,35 @@
+/**
+ * @author Luciano Graziani @lgraziani2712
+ * @license {@link http://www.opensource.org/licenses/mit-license.php|MIT License}
+ *
+ * @flow
+ */
+/* eslint-disable no-magic-numbers, object-property-newline */
+export default {
+  difficulty: 'normal',
+  engineData: {
+    canvas: {
+      height: 480,
+      width: 400,
+    },
+    width: 5,
+    height: 5,
+    margin: 10,
+    size: 64,
+    path: [
+      ['2,4', { top: true }],
+      ['2,3', { bottom: true, top: true }],
+      ['2,2', { bottom: true, top: true }],
+      ['2,1', { bottom: true, top: true }],
+      ['2,0', { bottom: true, top: true }],
+    ],
+    access: '2,4',
+    exits: ['2,0'],
+    actorExitIdx: 0,
+    platformCoords: '2,4',
+  },
+  numericLineData: {
+    statics: [0, null, 4, 5, 6, 7, 8, 9, 10, 11],
+    accesses: [1],
+  },
+};
---
+++ activities/MultipliersAndDivisors/engine/components/mazeGenerator.stories.jsx
@@ -0,0 +1,31 @@
+/**
+ * @author Luciano Graziani @lgraziani2712
+ * @license {@link http://www.opensource.org/licenses/mit-license.php|MIT License}
+ *
+ * @flow
+ */
+/* eslint-disable no-magic-numbers, */
+import React from 'react';
+
+import { storiesOf } from 'test/storybook-facades';
+import PixiWrapper from 'test/PixiWrapper';
+
+import metadata from '../../__mocks__/metadata';
+
+import mazeGenerator from './mazeGenerator';
+
+const { engineData } = metadata;
+const WIDTH = engineData.canvas.width;
+const HEIGHT = engineData.canvas.height;
+
+const basicMaze = mazeGenerator(engineData);
+
+storiesOf('MultipliersAndDivisors/engine/components/mazeGenerator', module)
+  .add('basic Maze', () => (
+    <PixiWrapper
+      component={basicMaze.view}
+      isContainer={true}
+      height={HEIGHT}
+      width={WIDTH}
+    />
+  ));
